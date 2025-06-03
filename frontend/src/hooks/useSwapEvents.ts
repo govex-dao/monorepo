445da70f7a68f934cdc9a01fc097483084b46017
mgavillo <marie.gavillon@gmail.com>
2025-06-04 03:36:53 +0400

    clean up and divide in multiple components

--- frontend/src/hooks/useSwapEvents.ts
+++ frontend/src/hooks/useSwapEvents.ts
@@ -22,7 +22,6 @@ export const useSwapEvents = (
   proposalId: string,
   options?: UseSwapEventsOptions,
 ) => {
-
   return useQuery<SwapEvent[], Error>({
     queryKey: ["swaps", proposalId],
     queryFn: async () => {
